a247c439556cee9000f9f6846b9b5ce8ee2b4d8a
Siddiqa Rahmat <siddiqarahmat532@gmail.com>
2026-05-23 23:36:25 +0530

    We learn npm , parcel

---
+++ Episode 2 - Igniting our App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "This is Namaste React");
+
+console.log(heading);  //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
